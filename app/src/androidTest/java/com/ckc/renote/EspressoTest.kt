4946f2f369a878ac0bba9e5e9d3d289ef047dfcd
Colin He <hcolin88@gmail.com>
2022-02-18 01:07:28 -0500

    Added framework for fourth test, some fixes for prevous tests

--- app/src/androidTest/java/com/ckc/renote/EspressoTest.kt
+++ app/src/androidTest/java/com/ckc/renote/EspressoTest.kt
@@ -1,6 +1,5 @@
 package com.ckc.renote
 
-import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
@@ -10,32 +9,16 @@ import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.*
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.TypeSafeMatcher
 import org.hamcrest.core.AllOf.allOf
 import org.junit.Test
-
-fun withIndex(matcher: Matcher<View?>, index: Int): Matcher<View?>? {
-    return object : TypeSafeMatcher<View?>() {
-        var currentIndex = 0
-        override fun describeTo(description: Description) {
-            description.appendText("with index: ")
-            description.appendValue(index)
-            matcher.describeTo(description)
-        }
-
-        override fun matchesSafely(view: View?): Boolean {
-            return matcher.matches(view) && currentIndex++ == index
-        }
-    }
-}
+import java.lang.Thread.sleep
 
 class EspressoTest {
     @Test
     fun testSettingsButton() {
         Intents.init()
         ActivityScenario.launch(MainActivity::class.java)
+        sleep(1000)
         onView(withId(R.id.action_settings)).perform(click())
         intended(hasComponent(SettingsActivity::class.java.name))
         Intents.release()
@@ -45,8 +28,12 @@ class EspressoTest {
     fun testEnteringText() {
         Intents.init()
         ActivityScenario.launch(MainActivity::class.java)
+        sleep(500)
         Espresso.closeSoftKeyboard()
+        val storeId = "111\n"
+        sleep(5000)
         onView(allOf(withId(R.id.edit_text1), isDisplayed()))
+            .perform(scrollTo())
             .perform(typeText("Does this work?"))
             .check(matches(withText("Does this work?")))
         Intents.release()
@@ -57,14 +44,39 @@ class EspressoTest {
     fun testSettingsPreservesText() {
         Intents.init()
         ActivityScenario.launch(MainActivity::class.java)
+        sleep(500)
         Espresso.closeSoftKeyboard()
+        sleep(500)
         onView(allOf(withId(R.id.edit_text1), isDisplayed()))
+            .perform(scrollTo())
             .perform(typeText("This text should be preserved"))
 
         onView(withId(R.id.action_settings)).perform(click())
         Espresso.pressBack()
         onView(allOf(withId(R.id.edit_text1), isDisplayed()))
             .check(matches(withText("This text should be preserved")))
+
+        Intents.release()
+    }
+
+    // Basic test to check saving and loading
+    @Test
+    fun testSavingAndLoading() {
+        Intents.init()
+        ActivityScenario.launch(MainActivity::class.java)
+        Espresso.closeSoftKeyboard()
+        onView(allOf(withId(R.id.edit_text1), isDisplayed()))
+            .perform(scrollTo())
+            .perform(typeText("Let's see if this saves properly"))
+
+        // Click on the three bars
+        // then switch to another file
+        // and then back to the original one
+        // and check if the text is still there
+        // I just can't find the damn IDs, sadge
+
+        onView(allOf(withId(R.id.edit_text1), isDisplayed()))
+            .check(matches(withText("Let's see if this saves properly")))
 
         Intents.release()
     }
