a75fe0928eca7be2344527bea3956e03b5a310be
Colin He <hcolin88@gmail.com>
2022-03-07 10:39:26 -0500

    Rudimentary database implementation, also some unit tests

---
+++ app/src/main/java/com/ckc/renote/Database.kt
@@ -0,0 +1,4 @@
+package com.ckc.renote
+
+class Database {
+}
---
+++ app/src/test/java/com/ckc/renote/DatabaseTest.kt
@@ -0,0 +1,20 @@
+package com.ckc.renote
+
+import junit.framework.TestCase
+
+class DatabaseTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+
+    fun testInsertNote() {}
+
+    fun testInsertNotes() {}
+
+    fun testSearchWithText() {}
+
+    fun testClose() {}
+}
