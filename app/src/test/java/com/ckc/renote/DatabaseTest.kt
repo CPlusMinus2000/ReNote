7191c14908d75a5c1941d0550a59912cefdc119c
Colin He <hcolin88@gmail.com>
2022-03-07 10:40:30 -0500

    Oops here are the database modifications

--- app/src/test/java/com/ckc/renote/DatabaseTest.kt
+++ app/src/test/java/com/ckc/renote/DatabaseTest.kt
@@ -3,18 +3,29 @@ package com.ckc.renote
 import junit.framework.TestCase
 
 class DatabaseTest : TestCase() {
+    private var db: Database? = null
 
     public override fun setUp() {
         super.setUp()
+        db = Database("jdbc:sqlite:test.db")
     }
 
-    public override fun tearDown() {}
+    public override fun tearDown() {
+        db!!.close()
+    }
 
-    fun testInsertNote() {}
+    fun testInsertNote() {
+        val testNote = Note(
+            "This is a test", "test", System.currentTimeMillis(),
+            System.currentTimeMillis(), null
+        )
+
+        val id = db!!.insertNote(testNote)
+        val ret = db!!.selectWithId(id)
+        assertEquals(testNote, ret)
+    }
 
     fun testInsertNotes() {}
 
     fun testSearchWithText() {}
-
-    fun testClose() {}
 }
