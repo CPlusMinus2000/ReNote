05816ed7ffcc3e7a903faac052e52353c3d33f03
Adam LaPorta <ajrlapor@uwaterloo.ca>
2022-03-11 00:00:45 -0500

    Unit tests in NoteRoomDatabaseTest.kt

--- app/src/androidTest/java/com/ckc/renote/NoteRoomDatabaseTest.kt
+++ app/src/androidTest/java/com/ckc/renote/NoteRoomDatabaseTest.kt
@@ -9,19 +9,59 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class NoteRoomDatabaseTest : TestCase() {
-    private lateinit var noteRoomDatabase: NoteRoomDatabase
-    private lateinit var noteDao: NoteDao
+    private var noteRoomDatabase: NoteRoomDatabase = NoteRoomDatabase.getDatabase(ApplicationProvider.getApplicationContext<Context>())
+    private var dao: NoteDao = noteRoomDatabase.noteDao()
+    private val noteName = "test"
+    private val bookName = "CS"
+    private val testNote = Note(
+        "This is a test", noteName, System.currentTimeMillis(),
+        System.currentTimeMillis(), 0, bookName
+    )
+    private val testNotebook = Notebook(
+        bookName, 0
+    )
 
     public override fun setUp() {
         super.setUp()
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        noteRoomDatabase = NoteRoomDatabase.getDatabase(context)
-        noteDao = noteRoomDatabase.noteDao()
     }
 
     @Test
     fun testGetNoteDao() {
         assertNotNull(noteRoomDatabase.noteDao())
+    }
+
+    @Test
+    fun testInsertNote() {
+        dao.insert(testNote)
+        val ret = dao.findByName(noteName)
+        assertEquals(testNote, ret)
+    }
+
+    @Test
+    fun testInsertNotebook() {
+        dao.insertNotebook(testNotebook)
+        val ret = dao.findNotebookByName(bookName)
+        assertEquals(testNotebook, ret)
+    }
+
+    @Test
+    fun testDeleteNote() {
+        dao.insert(testNote)
+        val ret = dao.findByName(noteName)
+        assertEquals(testNote, ret)
+        assertEquals(1, dao.noteExists(noteName))
+        dao.delete(testNote)
+        assertEquals(0, dao.noteExists(noteName))
+    }
+
+    @Test
+    fun testDeleteNotebook() {
+        dao.insertNotebook(testNotebook)
+        val ret = dao.findNotebookByName(bookName)
+        assertEquals(testNotebook, ret)
+        dao.deleteNotebook(testNotebook)
+        val ret2 = dao.findNotebookByName(bookName)
+        assertEquals(null, ret2)
     }
 
     public override fun tearDown() {}
